52b587561200ee010234f5c5a1e32efba6da4fd5
Olya Dyatlova <kuzhal97@gmail.com>
2020-05-29 18:21:13 +0300

    service

--- src/main/java/com/dyatlova/security/service/controller/UserController.java
+++ src/main/java/com/dyatlova/security/service/controller/UserController.java
@@ -1,10 +1,5 @@
 package com.dyatlova.security.service.controller;
 
-import com.dyatlova.security.model.User;
-import com.dyatlova.security.service.SecurityService;
-import com.dyatlova.security.service.UserService;
-import com.dyatlova.security.validator.UserValidator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
